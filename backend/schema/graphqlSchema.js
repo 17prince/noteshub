f1a1eb6bcf91a34ef3bdcd607c9c07acf7b72f8a
Prince <princelalla24@gmail.com>
2024-01-07 11:30:18 +0530

    generate mutation for motes[create, delete, update]

--- backend/schema/graphqlSchema.js
+++ backend/schema/graphqlSchema.js
@@ -1,4 +1,4 @@
-const notes = require("../sample/notes");
+const Notes = require("../models/noteModel");
 
 exports.typeDefs = `
 
@@ -15,16 +15,50 @@ type Query {
     notes: [Note], 
 }
 
+
+type Mutation{
+  createNote(title : String!, content : String!) : Note!
+  updateNote(id : ID!) : Note!
+  deleteNote(id : ID!) : Note!
+
+}
 `;
 
 exports.resolvers = {
   Query: {
-    note: (root, args) => {
-      return notes.find((note) => (note.id === args.id ? note : null));
+    note: async (root, args) => {
+      return await Notes.findById(args.id);
     },
-    notes: () => notes,
-    noteTitle: (_, { title }) => {
-      return notes.find((note) => (note.title === args.title ? note : null));
+    notes: async () => await Notes.find(),
+    noteTitle: async (_, { title }) => {
+      return await notes.find({ title });
+    },
+  },
+
+  Mutation: {
+    createNote: async (_, { title, content }) => {
+      if (!title || !content) return "Empty field";
+
+      const newNote = await Notes.create({
+        title,
+        content,
+        createdAt: Date.now(),
+      });
+
+      return newNote;
+    },
+
+    updateNote: async (_, { id, title, content }) => {
+      if (!title || !content) return "Empty field";
+
+      let note = await Notes.findById(id);
+
+      if (!note) return "Note with this ID not found";
+
+      note.title = title;
+      note.content = content;
+
+      return note.save();
     },
   },
 };
